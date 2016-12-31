0001ac48fd41aaa89f8ccf03c364b2bc55c5a7c7
shlo <shlo.sam@gmail.com>
2016-12-31 11:51:51 +0800

    history rating display

--- static/src/js/movieRecomm/controllers/index.js
+++ static/src/js/movieRecomm/controllers/index.js
@@ -2,99 +2,102 @@ require('../services');
 
 angular.module('MovieRecommApp')
     .controller(
-        'MainCtrl',
-        [
-            '$scope',
-            '$http',
-            '$uibModal',
-            'restSrv',
-            function ($scope, $http, $uibModal, restSrv) {
-                $scope.listSizes = [
-                    20,
-                    60,
-                    100,
-                ]
-                $scope.historyMode = false;
+	'MainCtrl',
+	[
+	    '$scope',
+	    '$http',
+	    '$uibModal',
+	    'restSrv',
+	    function ($scope, $http, $uibModal, restSrv) {
+		$scope.listSizes = [
+		    20,
+		    60,
+		    100,
+		]
+		$scope.historyMode = false;
 		$scope.listMovies = listMovies;
-                $scope.auth = window.auth;
+		$scope.auth = window.auth;
 
-                $scope.openRatingModal = function (item) {
-                    console.log(item);
-                    var modalInst = $uibModal.open(
-                        {
-                            template: require('./modal.html'),
-                            controller: 'ModalCtrl',
-                            resolve: {
-                                item: function () {
-                                    return item;
-                                },
-                                historyMode: $scope.historyMode
-                            }
-                        }
-                    );
-                    modalInst.result.then(function (rating) {
-                        if (typeof rating === 'undefined') return;
-                        $http.post(
-                            '/rating/',
-                            {item: item.id, rating: rating}
-                        )
-                            .success(function (resp) {
-                                console.log('post suc:', resp);
-                            })
-                            .error(function (resp) {
-                                console.error('post err:', resp);
-                            })
-                        ;
+		$scope.openRatingModal = function (item) {
+		    console.log(item);
+		    var modalInst = $uibModal.open(
+			{
+			    template: require('./modal.html'),
+			    controller: 'ModalCtrl',
+			    resolve: {
+				item: function () {
+				    return item;
+				},
+				historyMode: $scope.historyMode
+			    }
+			}
+		    );
+		    modalInst.result.then(function (rating) {
+			if (typeof rating === 'undefined') return;
+			$http.post(
+			    '/rating/',
+			    {item: item.id, rating: rating}
+			)
+			    .success(function (resp) {
+				console.log('post suc:', resp);
+			    })
+			    .error(function (resp) {
+				console.error('post err:', resp);
+			    })
+			;
 
-                        console.log(item.id, rating);
-                    });
-                };
+			console.log(item.id, rating);
+		    });
+		};
 
-                $scope.onModeChange = function () {
-                    console.log('mode: ', $scope.historyMode);
+		$scope.onModeChange = function () {
+		    console.log('mode: ', $scope.historyMode);
 		    if ($scope.historyMode) listHistory();
 		    else listMovies(20);
-                };
+		};
 
-                $scope.listMovies(20);
+		$scope.listMovies(20);
 
 		function listHistory () {
-                    restSrv.listHistory()
-                        .success(function (resp) {
-                            console.log(resp);
-                            $scope.items = resp.items;
-                        })
-                    ;
-                }
+		    restSrv.listHistory()
+			.success(function (resp) {
+			    console.log(resp);
+			    $scope.items = resp.items;
+			})
+		    ;
+		}
 
-                function listMovies (count) {
-                    restSrv.listMovies(count)
-                        .success(function (resp) {
-                            console.log(resp);
-                            $scope.items = resp.items;
-                        })
-                    ;
-                }
-            }
-        ]
+		function listMovies (count) {
+		    restSrv.listMovies(count)
+			.success(function (resp) {
+			    console.log(resp);
+			    $scope.items = resp.items;
+			})
+		    ;
+		}
+	    }
+	]
     )
     .controller('ModalCtrl',
-        [
-            '$scope',
-            '$uibModalInstance',
-            'item',
-            'historyMode',
-            function ($scope, $uibModalInstance, item, historyMode) {
-                $scope.item = item;
-                $scope.ok = function () {
-                    console.log('mode: ', historyMode);
-                    console.log('rating: ', $scope.rating);
-                    $uibModalInstance.close($scope.rating);
-                };
-                $scope.cancel = function () {
-                    $uibModalInstance.dismiss('cancel');
-                };
-            }
-        ]
+	[
+	    '$scope',
+	    '$uibModalInstance',
+	    'item',
+	    'historyMode',
+	    function ($scope, $uibModalInstance, item, historyMode) {
+		$scope.item = item;
+		$scope.auth = window.auth;
+		$scope.historyMode = historyMode;
+		$scope.rating = item.rating;
+		$scope.ok = function () {
+		    console.log('mode: ', historyMode);
+		    console.log('rating: ', $scope.rating);
+		    $uibModalInstance.close($scope.rating);
+		};
+		$scope.cancel = function () {
+		    $uibModalInstance.dismiss('cancel');
+		};
+	    }
+	]
     )
 ;
